a7875dfe669527ec30be10234346438043818564
Brad-Y <80384870+Brad-Y@users.noreply.github.com>
2021-03-19 21:36:47 +0800

    Update and rename post.js to admin and post.js

--- backend/user/admin and post.js
+++ backend/user/admin and post.js
@@ -3,6 +3,71 @@ package user;
 import "database/sql";
 import "encoding/json";
 
+var Admin={
+    id: 'admin',
+    password:'123456qwe',
+    delete: function(post_id){
+        my_delete(post,{_id:"post_id"})//Database operation
+    },
+    ban: function(uid){
+        my_update(user,{_id:uid},{is_banned:true})//Database operation
+    },
+    announce: function(annoucement,user_id){
+        alert(announcement);
+    },
+    login: function(id,password){
+        if (this.id===id&&this.password===password){
+            return 1;
+        }
+        else return 0;
+    },
+    logout: function(){
+        //return to homepage
+    }
+}
+
+function createAdmin(id,password){
+    var admin=Object.create(Admin);
+    admin.id=id;
+    admin.password=password;
+}
+
+var Post={
+    author: 'user',
+    id: 'id',
+    type: 0,//0 no image, 1 image
+    status: 0, //0 draft, 1 sent.
+    title: 'title',
+    text: 'text',
+    image: image,//image?
+    tag: [],
+    postTime: Date,
+    comment:[],
+    likes:0,
+    sendNotification: function(user_id){
+
+    }
+}
+
+function createPost(author,id,type,status,title,text,image,tag,){
+    var post=Object.create(Post);
+    post.author=author;
+    post.id=id;
+    post.type=type;
+    post.status=status;
+    post.title=title;
+    post.text=text;
+    post.image=image;
+    post.tag=tag;
+    post.sendNotification();
+}
+
+var Comment={
+    author: 'id',
+    postTime: Date,
+    content:'content',
+}
+
 function postGetAPI (){
 	postTitle  `json:"postTitle"`
 	postID    `json:"postID"`
