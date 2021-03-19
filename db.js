daca5f81b57061e3a085a0d6966f858b8b257906
toughguy1999 <80830771+toughguy1999@users.noreply.github.com>
2021-03-19 21:12:37 +0800

    Add files via upload

--- db.js
+++ db.js
@@ -10,19 +10,52 @@ mongoose.connect(url, { useUnifiedTopology: true }, function (err) {
     mongoose.connection.close();
 });
 
-// define schema of the database(not all fields are pulled in)
+// define schema of the database
 const user = mongoose.model("user",{
-    uname:{},
-    pwd:{}
+    uname:String,
+    pwd:String,
+    email:String,
+    Profile:{
+        user_name:String,
+        Gender:String,
+        Birth:String,
+        Introduction:String,
+        picture:{}
+    },
+    Liked:[],
+    Collected:[],
+    Post:[],
+    Message:{
+        sender:String,
+        Type:Number,   //(like,collect,comment,chat,announce,complain)
+        Text:String
+    },
+    Black_list:[],
+    is_admin:Boolean,
+    is_active:Boolean,
+    is_banned:Boolean
     }
 )
 
-const post = mongoose.model("post",{
-    author:{},
-    img:{},
-    text:{}
+const post = mongoose.model("post", {
+    Author: {
+        _id: String,
+        uname: String
+    },
+    Img: {},
+    Text: String,
+    Post_time: Date,
+    Read: Number,
+    Like: Number,
+    Collect: Number,
+    Tag: [],
+    Status: Number,
+    Comments: {
+        aid: String,  // there may include all info that need to be displayed
+        Time: Date,
+        Text: String
     }
-)
+})
 
 const tag = mongoose.model("tag",{
     name:String
@@ -30,21 +63,19 @@ const tag = mongoose.model("tag",{
 
 // define basic functions
 // insert
+my_insert = function (collection,doc) {
+    const insertObj = new collection(doc)
+    insertObj.save()
+        .then(res=>{
+            console.log(res)
+            return
+        })
+        .catch(err=>{
+            console.log("insert failed"+err)
+            return false
+        })
+}
 
-const insertObj = new post({
-    author: "Allen5",
-    text: "123123"
-    }
-)
-insertObj.save()
-    .then(res=>{
-        console.log(res)
-        return
-    })
-    .catch(err=>{
-        console.log("insert failed"+err)
-        return false
-    })
 
 // find
 my_find=function (collection,type,filter) {  // e.g. filter = {uname:"Allen2"}, filter = {uname:/Allen/}
@@ -74,30 +105,32 @@ my_find=function (collection,type,filter) {  // e.g. filter = {uname:"Allen2"}, 
 }
 
 // update
-
-user.updateOne({uname:"Allen2"} ,{uname:"Allen3"})
-    .then(res=>{
-        console.log(res)
-        return
-    })
-    .catch(err=>{
-        console.log("find failed"+err)
-        return false
-    }
-)
+my_update=function (collection,filter,update) {
+    collection.updateOne(filter,update)
+        .then(res => {
+            console.log(res)
+            return
+        })
+        .catch(err => {
+                console.log("update failed" + err)
+                return false
+            }
+        )
+}
 
 
 // delete
-user.deleteOne({uname:"Allen2"} )
-    .then(res=>{
-        console.log(res)
-        return
-    })
-    .catch(err=>{
-            console.log("find failed"+err)
-            return false
-        }
-    )
-
+my_delete=function (collection,filter) {
+    collection.deleteOne(filter)
+        .then(res => {
+            console.log(res)
+            return
+        })
+        .catch(err => {
+                console.log("delete failed" + err)
+                return false
+            }
+        )
+}
 
 
