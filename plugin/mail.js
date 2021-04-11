1a68f2eba35257a0686ea6319f400ca58de8bf15
jqye9 <63307003+jqye9@users.noreply.github.com>
2021-04-11 19:52:07 +0800

    Create mail.js

---
+++ plugin/mail.js
@@ -0,0 +1,59 @@
+//npm i nodemailer nodemailer-smtp-transport -save //安装模块
+
+//引入模块
+const nodemailer = require('nodemailer')
+const smtpTransport = require('nodemailer-smtp-transport')
+
+//创建连接对象
+const transport = nodemailer.createTransport(smtpTransport({
+    host: 'smtp.163.com', // 服务 由于我用的163邮箱
+    port: 465, // smtp端口 默认无需改动
+    secure: true,
+    auth: {
+      user: 'crackerlink@163.com', // 用户名
+      pass: 'xxxxxxxxx' // SMTP授权码
+    }
+}));
+
+//生成随机验证码函数及邮箱验证正则
+const randomFns=()=> { // 生成6位随机数
+    let code = ""
+    for(let i= 0;i<6;i++){
+        code += parseInt(Math.random()*10)
+    }
+    return code 
+}
+const regEmail=/^([a-zA-Z0-9]+[_|\_|\.]?)*[a-zA-Z0-9]+@([a-zA-Z0-9]+[_|\_|\.]?)*[a-zA-Z0-9]+\.[a-zA-Z]{2,3}$/ //验证邮箱正则
+
+//建立连接发送验证码
+exports.send=function (){
+let EMAIL=req.body.e_mail //req为请求体对象 我使用的是post请求方式，所以通过req.body获取用户提交的邮箱
+if (regEmail.test(EMAIL)){  //邮箱验证通过
+  let code=randomFns()
+  transport.sendMail({
+    from: 'crackerlink@163.com', // 发件邮箱
+    to: EMAIL, // 收件列表
+    subject: '验证你的电子邮件', // 标题
+    html: `
+    <p>你好！</p>
+    <p>您正在注册Cracker社区账号</p>
+    <p>你的验证码是：<strong style="color: #ff4e2a;">${code}</strong></p>
+    <p>***该验证码5分钟内有效***</p>` // html 内容
+  }, 
+  function(error, data) {
+    assert(!error,500,"发送验证码错误！")
+    transport.close(); // 如果没用，关闭连接池
+  })
+};
+//....验证码发送后的相关工作 
+//将邮箱和验证码保存到Code中，同时保证这条记录的唯一性。并在5分钟内将这条记录删除
+/*const Code = require("../models/Code")
+const e_mail = EMAIL
+await Code.deleteMany({e_mail}) //删除该旧的验证码，保证该邮箱是最新的验证码有效
+const [data] = await Code.insertMany({e_mail,veri_code:code}) //插入新邮箱验证码组合
+setTimeout(async ()=>{    //5分钟后删除
+    await Code.deleteMany({e_mail})
+},1000*60*5)
+}else{
+    assert(false,422,'请输入正确的邮箱格式！')
+}*/
