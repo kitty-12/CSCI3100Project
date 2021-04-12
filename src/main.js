be4b31a7fc97bf0e3494958cd27643ba9827ab48
kitty-12 <62559042+kitty-12@users.noreply.github.com>
2021-04-12 17:55:01 +0800

    Update main.js

--- src/main.js
+++ src/main.js
@@ -1,8 +1,33 @@
-import Vue from 'vue'
-import App from './App.vue'
+import App from "./App.vue";
+import ElementUI from "element-ui";
+import "element-ui/lib/theme-chalk/index.css";
+import VueRouter from "vue-router";
+import VueResource from "vue-resource";
+import axios from "axios";
+axios.defaults.baseURL = "http://localhost:3000";
 
-Vue.config.productionTip = false
+Vue.config.productionTip = false;
+
+Vue.use(ElementUI);
+Vue.use(VueRouter);
+Vue.use(VueResource);
+
+Vue.prototype.$axios = axios;
+
+const router = new VueRouter({
+  mode: "history",
+  routes: [
+    {path: "/", redirect: "/login" },
+    {path: "/login", component: () => import("@/components/login.vue")},
+    {path: "/home", component: () => import("@/components/home.vue")},
+    {path: "/register", component: () => import("@/components/register.vue")},
+    {path: "/upload", component: () => import("@/components/makeFanwork.vue")},
+    {path: "/test", component: () => import("@/components/test.vue")}
+  ]
+});
+
 
 new Vue({
   render: h => h(App),
+  router
 }).$mount('#app')
