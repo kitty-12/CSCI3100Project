5ac76c81cba44bb580deb1948e457c6cd4600b15
toughguy1999 <80830771+toughguy1999@users.noreply.github.com>
2021-04-12 23:10:49 +0800

    Update route.js

--- backend/route.js
+++ backend/route.js
@@ -66,6 +66,7 @@ router.post('/admin/editProfile', cors(),function (req, res) {
         docs[0].user_name = info.user_name
         docs[0].introduction = info.Introduction
         docs[0].picture = info.picture
+        docs[0].banner = info.banner
         db.User(docs[0]).save(function (err) {
             if (err) {
                 res.status(500).send()
