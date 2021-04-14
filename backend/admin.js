8ea4e357543a5b0e5b5ac0b084924ec8a920ee07
toughguy1999 <80830771+toughguy1999@users.noreply.github.com>
2021-04-14 20:01:42 +0800

    Add files via upload

---
+++ backend/admin.js
@@ -0,0 +1,29 @@
+const express = require('express')
+const router = express.Router()
+const db = require('./database/db')
+cors = require("cors")
+
+// ban
+router.post('/admin/admin/ban', cors(),function (req, res) {
+    db.connect()
+    db.User.update({email: req.body},{is_banned:true}, function (err) {
+        if (err) {
+            console.log(err)
+            return
+        }
+    })
+})
+
+// announcement
+router.post('/admin/admin/announce', cors(),function (req, res) {
+    db.connect()
+    db.User.updateMany( {$push:{message: "Admin has posted a new announcement: " + req.body}},function (err, docs) {
+        if (err) {
+            console.log(err)
+            return
+        }
+
+    })
+})
+
+module.exports = router
