ef566e3367eaab7469a5f8fb2681d99c55bdf18b
kitty-12 <62559042+kitty-12@users.noreply.github.com>
2021-04-29 21:16:06 +0800

    Update admin.js

--- backend/admin.js
+++ backend/admin.js
@@ -4,24 +4,30 @@ const db = require('./database/db')
 cors = require("cors")
 
 // Forbid a user to comment
-router.post('/admin/ban', cors(),function (req, res) {
+router.get('/ban', cors(),function (req, res) {
     db.connect()
-    db.User.update({email: req.body},{is_banned:true}, function (err) {
+    console.log("req: ", req)
+    console.log(req.query)
+    db.User.update({email: req.query.input1},{is_banned:true}, function (err) {
         if (err) {
             console.log(err)
             return
         }
+
+        res.send()
     })
 })
 
 // Send a notification to all users
-router.post('/admin/announce', cors(),function (req, res) {
+router.get('/announce', cors(),function (req, res) {
     db.connect()
-    db.User.updateMany( {$push:{message: "Admin has posted a new announcement: " + req.body}},function (err, docs) {
+    console.log(req.query)
+    db.User.updateMany( {$push:{message: "Admin has posted a new announcement: " + req.query.input2}},function (err, docs) {
         if (err) {
             console.log(err)
             return
         }
+        res.send()
 
     })
 })
