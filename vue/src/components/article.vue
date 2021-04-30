5544a646720cbc06e781157284c0e7bdcd2feadc
kitty-12 <62559042+kitty-12@users.noreply.github.com>
2021-04-30 20:02:48 +0800

    Update article.vue

--- vue/src/components/article.vue
+++ vue/src/components/article.vue
@@ -1,3 +1,8 @@
+<!--
+    * @File Description: Article page, view the article, like, collect, comment on the article, add to blacklist
+    * @Last Edit Date: 2021/4/30
+-->
+
 <template>
     <div class="articlePage">
         <Navigator :uid="uid"></Navigator>
@@ -6,13 +11,17 @@
 
         <div class="info">
             <p class="info">{{article.author.uname}}</p>
+            <el-link type='primary' @click="$router.push({name:'user',params:{uid1:article.author.author_id,uid2:uid}})">See page</el-link>
             <p class="info">{{article.post_time}}</p>
 
             <div class="tags">
                     <el-tag
+                        class='tag'
                         v-for="tag in tags"
                         :key="tag"
-                        effect="plain">
+                        closable
+                        effect="plain"
+                        @close='addBlacklist(tag)'>
                         {{tag}}
                     </el-tag>
             </div>
@@ -20,10 +29,10 @@
 
         <div class="content">
             <el-image :src="imgsrc"></el-image>
-            <br>
-            <span  v-html="article.text">{{article.text}}</span>
+            <span  v-html="article.text" style="margin-top: 20px">{{article.text}}</span>
         </div>
 
+        <br>
         <div class="action">
             <el-badge :value=article.read class="item">
                 <el-button size="small" disabled round>View by {{article.read}}</el-button>
@@ -37,7 +46,8 @@
                 <el-button v-bind:class="{clicked: click2 == 1}" size="small" @click="collect" round>Collect</el-button>
             </el-badge>
         </div>
-
+        
+        <br>
         <div class="inputComment">
             <el-input
                 class="text"
@@ -47,10 +57,13 @@
                 v-model="comment"
                 style="width: 40%">
             </el-input>
-            <el-button size="small" @click="commentOn">Comment</el-button>
+            <br>
+            <el-button size="small" @click="commentOn" style="margin-top:20px">Comment</el-button>
         </div>
 
+        <br>
         <div class="comment_area">
+            <p>Comments:</p>
             <ul>
             <li v-for="showcomment in showcomments" :key="showcomment">
                 {{showcomment}}
@@ -63,7 +76,7 @@
 </template>
 
 <script>
-//import authorCard from './authorProfile.vue';
+
 import Navigator from './Navigator.vue';
 //import Comment from './comment.vue';
 
@@ -72,10 +85,10 @@ export default {
 
     data() {
         return {
-            click1: 0,
-            click2: 0,
-            htmlText:'',
-            imgsrc:"http://localhost:8081",
+            click1: 0, // check whether like is clicked
+            click2: 0, // check whether collect is clicked
+            htmlText:'', // the text of the article in html format
+            imgsrc:"http://localhost:8081",  // the img url
             article:{
                 author: {
                     author_id: '',
@@ -89,32 +102,73 @@ export default {
                 like: [],
                 collect: Number,
                 tag: [],
-                comments: [{
-                    author_id: String,  // there may include all info that need to be displayed
-                    time: Date,
-                    text: String
-                }]
+                comments: []
             },
-            comment: '',
-            tags:[],
-            article_id:'',
-            uid:'',
-            showcomments:[],
+            comment: '', // user input comment
+            tags:[],  // tags shown in the page
+            article_id:'', 
+            uid:'', // user id
+            showcomments:[], // comments shown in the comment_area
+            likenum:Number,
         }
     },
 
     components: {
-        //authorCard,C
         Navigator,
         //Comment,
     },
 
     created: function(){
+
+        // get the article id and uid passed from other components
         this.article_id = this.$route.params.article_id;
         console.log(this.article_id)
         this.uid = this.$route.params.uid;
         console.log(this.uid)
-        if(this.article_id =='undefined')
+        
+        // get article content
+        this.$http.post(
+            'http://localhost:3000/api/article/articlePage', 
+            {_id: this.article_id},
+            {emulateJSON: true})
+            .then(
+                function(res){
+                    this.article = res.body;
+                    //console.log(res.body.img[0]);
+                    console.log(res);
+                    this.imgsrc='http://localhost:8081'+res.body.img[0]
+                    console.log(this.imgsrc)
+                    this.article,this.tag = res.body.tag;
+                    this.article.img = res.body.img;
+                    this.article.like = res.body.like;
+                    this.tags = this.article.tag;
+                }
+            );
+        this.htmlText = this.article.text;
+        this.likenum = this.article.like.length
+
+        console.log(this.article.img)
+
+        //get comments of this article
+        this.$http.post(
+            "http://localhost:3000/api/article/articlePage1",
+            {_id: this.article_id},
+            {emulateJSON: true},
+        ).then(
+            function(res){
+            this.showcomments = res.body.comments;
+            //console.log(this.showcomments);
+            console.log(res);
+        })
+
+        //this.imgsrcs = this.article.img;
+        //console.log(this.htmlText)
+        //console.log(this.article)
+        //console.log(this.article.img[0])
+        //this.imgsrc = this.imgsrc + this.article.img[this.article.img.length-1]
+
+        // check whether article id undefined, if empty, push to main page
+        if(!this.article_id)
         {
             this.$alert('The article is empty. Please do NOT click on empty post. Return to mainpage', 'Tip', {
                 confirmButtonText: 'Confirm',
@@ -125,48 +179,14 @@ export default {
                     });
                 }   
             });
-            this.$route.push({name:"main",params: {uid: this.uid}})
+            this.$router.push({name:"main",params: {uid: this.uid}})
         }
-        
-        this.$http.post(
-            'http://localhost:3000/api/article/articlePage', 
-            {_id: this.article_id},
-            {emulateJSON: true})
-            .then(
-                function(res){
-                    this.article = res.body;
-                    console.log(res.body.img[0]);
-                    //console.log(res);
-                    this.imgsrc='http://localhost:8081'+res.body.img[0]
-                    console.log(this.imgsrc)
-                    this.article,this.tag = res.body.tag;
-                    this.article.img = res.body.img;
-                    this.article.like = res.body.like;
-                    this.tags = this.article.tag;
-                }
-            );
-        this.htmlText = this.article.text;
-
-        console.log(this.article_id)
-        this.$http.post(
-            "http://localhost:3000/api/article/articlePage1",
-            {_id: this.article_id},
-            {emulateJSON: true},
-        ).then(
-            function(res){
-            this.showcomments = res.body.comments;
-            console.log(this.showcomments);
-            console.log(res.body.comments);
-        })
-        //this.imgsrcs = this.article.img;
-        //console.log(this.htmlText)
-        //console.log(this.article)
-        //console.log(this.article.img[0])
-        //this.imgsrc = this.imgsrc + this.article.img[this.article.img.length-1]
 
     },
 
     methods: {
+
+        // like the article
         likeArticle(){
             if(this.click1 == 1)
             {
@@ -184,8 +204,6 @@ export default {
             let likeInfo = {
                 article_id: this.article_id,
                 likerid:this.uid,
-                aid:this.article_id,
-
             };//user id
 
             this.$http.post(
@@ -195,23 +213,32 @@ export default {
                 .then(
                     function(res){
                         if(res.ok){
-                            //this.article.like[this.article.like.length] = ''; //
+                            if(res.body == '0')
+                                this.article.like.push(''); //
                             this.click1 = 1;
-                            this.$notify({
-                                title: 'Message',
-                                message: 'Thanks for your like ♥',
-                                duration: 0
-                            });
+                            if(res.body == '0')
+                                this.$notify({
+                                    title: 'Message',
+                                    message: 'Thanks for your like ♥',
+                                    duration: 0
+                                });
+                            else{
+                                this.$notify({
+                                    title: 'Message',
+                                    message: 'You have liked this fanwork',
+                                    duration: 0
+                                    });
+                            }
                         }
                     }
                 );
         },
 
+        // make comments
         commentOn(){
             let commentInfo = {
                 user_id:this.uid,
                 article_id:this.article_id,
-                aid:this.article_id,
                 Time:Date,
                 Text:this.comment
             }
@@ -223,17 +250,41 @@ export default {
                 .then(
                     function(res){
                         if(res.ok){
-                            this.$notify({
+                            if(res.body.message =='ban')
+                            {
+                                this.$notify({
                                 title: 'Message',
-                                message: 'Comment Success ♥',
+                                message: 'You are not allowed to comment.',
                                 duration: 0
-                            });
+                                });
+                            }else{
+                                this.$notify({
+                                    title: 'Message',
+                                    message: 'Comment Success ♥',
+                                    duration: 0
+                                });
+
+                                //this.$router.push({name:'article',params:{uid:this.uid, article_id:this.article_id}})
+
+                                this.$http.post(
+                                    "http://localhost:3000/api/article/articlePage1",
+                                    {_id: this.article_id},
+                                    {emulateJSON: true},
+                                ).then(
+                                    function(res){
+                                        this.showcomments = res.body.comments;
+                                        console.log(this.showcomments);
+                                        console.log(res.body.comments);
+                                })
+                            }
                         }
+                        console.log(res)
                     }
                 );
-
+            
         },
 
+        // collect the article
         collect(){
             if(this.click2 == 1)
             {
@@ -259,16 +310,61 @@ export default {
                     function(res){
                         if(res.ok){
                             //this.article.like[this.article.like.length] = ''; //
-                            this.article.collect += 1;
+                            if(res.body =='0')
+                                this.article.collect += 1;
                             this.click2 = 1;
-                            this.$notify({
-                                title: 'Message',
-                                message: 'Thanks for your collecting ♥',
-                                duration: 0
-                            });
+                            if(res.body =='0')
+                                this.$notify({
+                                    title: 'Message',
+                                    message: 'Thanks for your collecting ♥',
+                                    duration: 0
+                                });
+                            else{
+                                this.$notify({
+                                    title: 'Message',
+                                    message: 'You have collectd this fanwork',
+                                    duration: 0
+                                });
+                            }
                         }
                     }
                 );
+        },
+
+        // add the tag to blacklist
+        addBlacklist(tag){
+            this.$confirm('Add to Blacklist', 'Tip', {
+                confirmButtonText: 'Yes',
+                cancelButtonText: 'Cancel',
+                type: 'warning'
+                }).then(() => {
+
+                    let info = {
+                        _id: this.uid,
+                        tag: tag
+                        }
+                    this.$http.post(
+                        'http://localhost:3000/api/user_page/addBlacklist',
+                        info,
+                        {emulateJSON: true})
+                    .then(
+                        function(res){
+                            if(res.ok){
+                                this.$notify({
+                                    title:'Message',
+                                    message: 'You have added this tag to blacklist :(',
+                                    duration: 0
+                                });
+                            }
+                        }
+                    );
+                }).catch(() => {
+                    this.$message({
+                        type: 'info',
+                        message: 'Cancelled'
+                    });          
+                });
+            
         }
     }
 }
@@ -281,5 +377,47 @@ export default {
     background-color: coral;
 }
 
+.tags{
+    margin: auto;
+    margin-bottom: 20px;
+}
+
+.tag{
+    margin-left: 10px;
+}
+
+.comment_area{
+    margin: auto;
+    margin-bottom: 20px;
+    margin-top:10px;
+    text-align: left;
+    align-items: left;
+    display: flex;
+    flex-direction: column;
+    width: 40%;
+}
+
+.content{
+    margin: auto;
+    margin-bottom: 20px;
+    margin-top:30px;
+    text-align: left;
+    align-items: left;
+    display: flex;
+    flex-direction: column;
+    width: 40%;
+}
+
+.inputComment{
+    display: flex;
+    margin:auto;
+    text-align: center;
+    align-items: center;
+    flex-direction: column;
+}
+
+.action .item{
+    margin-left: 40px;
+}
 </style>>
 
